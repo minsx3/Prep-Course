2953fcb70172fb2d9b4203b673a2d9a62509b4b0
Cristian <ctorresf93@gmail.com>
2022-05-17 20:29:35 -0400

    avance

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -35,8 +35,10 @@ function suma(x, y) {
   // "x" e "y" son números
   // Suma "x" e "y" juntos y devuelve el valor
   // Tu código:
-  
+  var suma = x + y;
+  return suma;  
 }
+suma
 
 function resta(x, y) {
   // Resta "y" de "x" y devuelve el valor
